142d891ac147c2b86971c8469046bfb70b60f574
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-25 01:04:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,13 @@ class Student
   end
   
   def self.new_from_db(row)
-    student = Student.new(row[0], row[1], row[2]) 
+    student = Student.new(row[0], row[1], row[2])
+    student
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
   end
   
   def update  
